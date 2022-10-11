eb3cd24045d70e8d01e82bdaa6773b8d69ecc771
skbarbee <barbee.s.k@gmail.com>
2022-10-11 14:26:18 -0400

    trying to cleanup routes

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -23,106 +23,32 @@ router.use((req, res, next) => {
 })
 
 // Routes
-
-// index ALL
-router.get('/', (req, res) => {
-	Example.find({})
-		.then(examples => {
-			const username = req.session.username
-			const loggedIn = req.session.loggedIn
-			
-			res.render('examples/index', { examples, username, loggedIn })
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// index that shows only the user's examples
-router.get('/mine', (req, res) => {
-    // destructure user info from req.session
-    const { username, userId, loggedIn } = req.session
-	Example.find({ owner: userId })
-		.then(examples => {
-			res.render('examples/index', { examples, username, loggedIn })
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// new route -> GET route that renders our page with the form
-router.get('/new', (req, res) => {
-	const { username, userId, loggedIn } = req.session
-	res.render('examples/new', { username, loggedIn })
-})
-
-// create -> POST route that actually calls the db and makes a new document
-router.post('/', (req, res) => {
-	req.body.ready = req.body.ready === 'on' ? true : false
-
+// update routine with new task
+//find routine by Id then CREATE a new task 
+router.post('/:id', (req, res) => {
+	const routineId= req.params.id
+	// req.body.ready = req.body.ready === 'on' ? true : false
 	req.body.owner = req.session.userId
-	Example.create(req.body)
-		.then(example => {
-			console.log('this was returned from create', example)
-			res.redirect('/examples')
+	const theTask = {task: req.body.task, complete: req.body.complete, type: req.body.type, owner: req.body.owner}
+	Routine.findByIdAndUpdate(routineId, req.body, { new: true } )
+	.populate("listItems.task")
+		.then(routine =>{
+			console.log("this is body", req.body)
+			routine.listItem.addToSet(req.body)
+			console.log("this is the list item", routine.listItem)
+			console.log("this is the new routine", routine)
+			console.log("this is the routine id", routine.id)
+			res.status(201).json({ routine: routine.toObject() })
+			
 		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// edit route -> GET that takes us to the edit form view
-router.get('/:id/edit', (req, res) => {
-	// we need to get the id
-	const exampleId = req.params.id
-	Example.findById(exampleId)
-		.then(example => {
-			res.render('examples/edit', { example })
-		})
+	
+	
 		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
+			// res.redirect(`/error?error=${error}`)
+			console.log(error)
 		})
 })
 
-// update route
-router.put('/:id', (req, res) => {
-	const exampleId = req.params.id
-	req.body.ready = req.body.ready === 'on' ? true : false
-
-	Example.findByIdAndUpdate(exampleId, req.body, { new: true })
-		.then(example => {
-			res.redirect(`/examples/${example.id}`)
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// show route
-router.get('/:id', (req, res) => {
-	const exampleId = req.params.id
-	Example.findById(exampleId)
-		.then(example => {
-            const {username, loggedIn, userId} = req.session
-			res.render('examples/show', { example, username, loggedIn, userId })
-		})
-		.catch((error) => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
-
-// delete route
-router.delete('/:id', (req, res) => {
-	const exampleId = req.params.id
-	Example.findByIdAndRemove(exampleId)
-		.then(example => {
-			res.redirect('/examples')
-		})
-		.catch(error => {
-			res.redirect(`/error?error=${error}`)
-		})
-})
 
 // Export the Router
 module.exports = router
