38751a44d143a0bdcf3e0ae05dfa261c75939b9c
skbarbee <barbee.s.k@gmail.com>
2022-10-17 13:13:14 -0400

    deployed URL

--- models/connection.js
+++ models/connection.js
@@ -1,22 +1,31 @@
-// make our .env variables available via process.env
-require('dotenv').config()
-// import mongoose
-const mongoose = require('mongoose')
+/////////////////////////////////////////////
+// import dependencies
+/////////////////////////////////////////////
+require("dotenv").config() // Load ENV Variables
+const mongoose = require("mongoose") // import mongoose
 
-// connect to the database
+/////////////////////////////////////////////
+// Database Connection
+/////////////////////////////////////////////
+// this is where we will set up our inputs for our database connect function
+const DATABASE_URL = process.env.DATABASE_URL
+const DEPLOYED_URL = process.env.DEPLOYED_URL
+// here is our DB config object
+const CONFIG = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+// establish our connection
+mongoose.connect(DEPLOYED_URL, CONFIG)
 
-mongoose.connect(process.env.DATABASE_URL, {
-	useUnifiedTopology: true,
-	useNewUrlParser: true,
-})
+// tell mongoose what to do with certain events
+// opens, disconnects, errors
+mongoose.connection
+    .on("open", () => console.log("Connected to Mongoose"))
+    .on("close", () => console.log("Disconnected from Mongoose"))
+    .on("error", (error) => console.log("An error occurred: \n", error))
 
-// save the connection in a variable
-const db = mongoose.connection
-
-// create some notification
-db.on('open', () => console.log('You are connected to mongo'))
-db.on('close', () => console.log('You are disconnected from mongo'))
-db.on('error', (error) => console.log(error))
-
-// export the connection
+/////////////////////////////////////////////
+// export our connection
+/////////////////////////////////////////////
 module.exports = mongoose
